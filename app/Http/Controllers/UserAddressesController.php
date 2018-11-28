f89f4bd381a12ea8877c4825d39e9fdc6b62c6e1
zs <zhengsheng0118@163.com>
2018-11-28 22:16:38 +0800

    初始化管理后台

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -62,7 +62,7 @@ class UserAddressesController extends Controller
     public function destroy(UserAddress $user_address)
     {
         $this->authorize('own', $user_address);
-        
+
         $user_address->delete();
         // 把之前的 redirect 改成返回空数组
         return [];
